fb9eab976c142142d033cf187e785e1e72143af7
Carlos Roberto Barbosa Júnior <carlosjr.if@gmail.com>
2020-02-14 14:19:29 -0300

    [ADD] Construindo o layout inicial

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é a seu animal favorito?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text("Resposta 1"),
+              onPressed: null
+            ),
+            RaisedButton(
+              child: Text("Resposta 2"),
+              onPressed: null
+            ),
+            RaisedButton(
+              child: Text("Resposta 3"),
+              onPressed: null
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
